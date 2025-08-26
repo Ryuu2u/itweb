027d024574ae7819fa751aca31d643fa112f3ceb
Ryuu2u <asriw12987c@gmail.com>
2025-08-26 15:52:25 +0700

    update database server

--- controls/fetchProduct.php
+++ controls/fetchProduct.php
@@ -4,6 +4,6 @@ include './controls/db.php';
 session_start();
 
 // ดึงข้อมูลสินค้าจากตาราง products
-$sql = "SELECT `id`, `product_name`, `description`, `price`, `created_at`, `product_image` FROM `products`";
+$sql = "SELECT `id`, `name`, `description`, `price`, `created_at`, `image` FROM `products`";
 $stmt = $pdo->prepare($sql); // ใช้ PDO ในการเตรียมค าสั่ง SQL
 $stmt->execute(); // รันคำสั่ง SQL
